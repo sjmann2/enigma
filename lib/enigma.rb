704c117b0c5a68770ea275551539f7491808afd5
Sid Mann <sjmann2@outlook.com>
2022-08-05 16:35:53 -0500

    progress on encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,4 @@
-require 'date'
+require "date"
 
 class Enigma
   attr_reader :characters
@@ -12,7 +12,7 @@ class Enigma
   end
 
   def key_generator
-    5.times.map{rand(10)}.join
+    5.times.map { rand(10) }.join
   end
 
   def offset_generator(date)
@@ -24,12 +24,47 @@ class Enigma
     # :encryption => the encrypted String
     # :key => the key used for encryption as a String
     # :date => the date used for encryption as a String in the form DDMMYY
+    # encryption = {
+    #   encryption:
+    #   key:
+    #   date:
+    # }
     offset = offset_generator(date)
-    
-    a_shift = (key[0] + key[1]).to_i + offset[0].to_i
-    b_shift = (key[1] + key[2]).to_i + offset[1].to_i
-    c_shift = (key[2] + key[3]).to_i + offset[2].to_i
-    d_shift = (key[3] + key[4]).to_i + offset[3].to_i
-            
+
+    a_shift = (key[0] + key[1]).to_i + offset[0].to_i #3
+    b_shift = (key[1] + key[2]).to_i + offset[1].to_i #27
+    c_shift = (key[2] + key[3]).to_i + offset[2].to_i #73
+    d_shift = (key[3] + key[4]).to_i + offset[3].to_i #20
+
+    message[0].each_char.with_index do |char, index|
+      #for A: start at 1 and go every 4th
+      #for B: start at 2 and go every 4th
+      #for C: start at 3 and go every 4th
+      #for D: start at 4 and go every 4th
+      a_shift char
+
+      message[0..-1].each_char.with_index do |char, index|
+        if index % 4 == 0
+          puts char
+        end
+      end
+      #start at index 0, iterate through the end, give me the
+      #character at index position that are divisible by 4
+      message[1..-1].each_char.with_index do |char, index|
+        if index % 4 == 0
+          puts char
+        end
+      end
+      message[2..-1].each_char.with_index do |char, index|
+        if index % 4 == 0
+          puts char
+        end
+      end
+      message[3..-1].each_char.with_index do |char, index|
+        if index % 4 == 0
+          puts char
+        end
+      end
+    end
   end
 end
