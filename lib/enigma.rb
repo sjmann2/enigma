cdc703118f7a88c62acdb7bf3826833ad659ab2e
Sid Mann <sjmann2@outlook.com>
2022-08-06 16:57:14 -0500

    helper methods for decrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -20,23 +20,20 @@ class Enigma
     squared_date[-4..-1]
   end
 
-  def shift_calculator(key = key_generator, date = date_generator)
+  def shift_calculator(key, date)
     offset = offset_calculator(date)
-  
-    shifts = { 
+
+    shifts = {
       a_shift: (key[0] + key[1]).to_i + offset[0].to_i,
       b_shift: (key[1] + key[2]).to_i + offset[1].to_i,
       c_shift: (key[2] + key[3]).to_i + offset[2].to_i,
-      d_shift: (key[3] + key[4]).to_i + offset[3].to_i
+      d_shift: (key[3] + key[4]).to_i + offset[3].to_i,
     }
   end
 
-  def encrypt(message, key = key_generator, date = date_generator)
-    message.downcase!
-    shifts = shift_calculator(key, date)
-
+  def find_character_index_values(message)
     character_index_values = []
-    #[7, 4, 11, 11, 14, 26, 22, 14, 17, 11, 3]
+
     message.each_char.with_index do |char, index|
       @characters.each_with_index do |character, character_index|
         if character == char
@@ -44,9 +41,14 @@ class Enigma
         end
       end
     end
+    character_index_values
+  end
+
+  def find_encrypted_index_values(message, key, date)
+    character_index_values = find_character_index_values(message)
+    shifts = shift_calculator(key, date)
 
     encrypted_index_values = []
-    #[10, 31, 84, 31, 17, 53, 95, 34, 20, 38, 76]
     character_index_values.each_with_index do |value, index|
       if index % 4 == 0
         encrypted_index_values << (value + shifts[:a_shift]) % 27
@@ -58,30 +60,14 @@ class Enigma
         encrypted_index_values << (value + shifts[:d_shift]) % 27
       end
     end
-
-    encrypted_message = encrypted_index_values.map { |position| @characters[position] }
-
-    encryption = {
-      :encryption => encrypted_message.join,
-      :key => key,
-      :date => date,
-    }
+    encrypted_index_values
   end
 
-  def decrypt(ciphertext, key, date = date_generator)
+  def find_decrypted_index_values(message, key, date)
+    character_index_values = find_character_index_values(message)
     shifts = shift_calculator(key, date)
 
-    character_index_values = []
-    #[10, 4, 3, 4, 17, 26, 14, 7, 20, 11, 22]
-    ciphertext.each_char.with_index do |char, index|
-      @characters.each_with_index do |character, character_index|
-        if character == char
-          character_index_values << character_index
-        end
-      end
-    end
     decrypted_index_value = []
-    #[7, 4, 11, 11, 14, 26, 22, 14, 17, 11, 3]
     character_index_values.each_with_index do |value, index|
       if index % 4 == 0
         decrypted_index_value << (value - shifts[:a_shift]) % 27
@@ -93,14 +79,32 @@ class Enigma
         decrypted_index_value << (value - shifts[:d_shift]) % 27
       end
     end
-    decrypted_message = decrypted_index_value.map { |position| @characters[position] }
-   
+    decrypted_index_value
+  end
+
+  def encrypt(message, key = key_generator, date = date_generator)
+    message.downcase!
+    shifts = shift_calculator(key, date)
+    encrypted_index_values = find_encrypted_index_values(message, key, date)
+    encrypted_message = encrypted_index_values.map { |position| @characters[position] }
+
+    encryption = {
+      :encryption => encrypted_message.join,
+      :key => key,
+      :date => date,
+    }
+  end
+
+  def decrypt(message, key, date = date_generator)
+    shifts = shift_calculator(key, date)
+    decrypted_index_values = find_decrypted_index_values(message, key, date)
+
+    decrypted_message = decrypted_index_values.map { |position| @characters[position] }
+
     decryption = {
       :decryption => decrypted_message.join,
       :key => key,
-      :date => date
+      :date => date,
     }
   end
 end
-
-#if you rotate the array, the original character index values stay the same
