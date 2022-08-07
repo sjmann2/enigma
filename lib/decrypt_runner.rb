280e80da99c26d1132ddb0b7135a23d0fe0475a1
Sid Mann <sjmann2@outlook.com>
2022-08-06 20:35:06 -0500

    decrypt runner class and incorporating decrypt runner into runner file

---
+++ lib/decrypt_runner.rb
@@ -0,0 +1,27 @@
+class DecryptRunner
+  attr_reader :read_file_path,
+              :write_file_path,
+              :key,
+              :date
+
+  def initialize(read_file_path, write_file_path, key, date)
+    @read_file_path = read_file_path
+    @write_file_path = write_file_path
+    @key = key
+    @date = date
+    @enigma = Enigma.new
+  end
+
+  def run
+    encrypted = File.open(@read_file_path, "r")
+    message = encrypted.read
+    encrypted.close
+    
+    decrypted_text = @enigma.decrypt(message, key = (@key), date = (@date))
+    decrypt = File.open(@write_file_path, "w")
+    decrypt.write(decrypted_text)
+    puts "created 'decrypted.txt' with the key #{key} and date #{date}"
+    
+    decrypt.close
+  end
+end
--- spec/decrypt_runner_spec.rb
+++ spec/decrypt_runner_spec.rb
@@ -10,9 +10,9 @@ describe DecryptRunner do
     expect(@decrypt_runner).to be_a(DecryptRunner)
   end
 
-  it 'has a read file path, output file path, key, and date' do
+  it 'has a read file path, write file path, key, and date' do
     expect(@decrypt_runner.read_file_path).to eq("encrypted.txt")
-    expect(@decrypt_runner.output_file_path).to eq("decrypted.txt")
+    expect(@decrypt_runner.write_file_path).to eq("decrypted.txt")
     expect(@decrypt_runner.key).to eq(82648)
     expect(@decrypt_runner.date).to eq(240818)
   end
