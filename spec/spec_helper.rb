4ff11085e5286a1de1f35ad8650fe16608e9cc1a
Sid Mann <sjmann2@outlook.com>
2022-08-08 09:37:15 -0500

    shift calculator spec to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,6 @@ require "simplecov"
 SimpleCov.start
 
 require_relative "enigma_spec"
+require_relative "shift_calculator_spec"
 require_relative "decrypt_runner_spec"
 require_relative "encrypt_runner_spec"
