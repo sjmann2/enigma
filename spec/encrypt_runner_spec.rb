6a94054c26f0dc64d0fece47490b4f9e8dc4ed28
Sid Mann <sjmann2@outlook.com>
2022-08-09 09:14:43 -0500

    add stub tests for run methods

--- spec/encrypt_runner_spec.rb
+++ spec/encrypt_runner_spec.rb
@@ -16,6 +16,21 @@ describe EncryptRunner do
   end
 
   it "has a run method" do
-    allow(@encrypt_runner).to receive(:run).and_return("created 'encrypted.txt' with the key 02715 and date 040895")
+    allow_any_instance_of(EncryptRunner).to receive(:run).and_return("created 'decrypted.txt' with the key 02715 and date 040895")
   end
+
+  it "can read a file" do
+    message_file = double(File, read: "stubbed read")
+    allow_any_instance_of(File).to receive(:open).and_return("stubbed read")
+  end
+
+  it "can encrypt a message" do
+    message = "hello world"
+    key = double("key")
+    enigma = double("enigma")
+    date = double("date")
+    allow(enigma).to receive(:encrypt).and_return("keder ohulw")
+    expect(enigma.encrypt(message, key, date)).to eq("keder ohulw")
+  end
+
 end
